b736bd33198dcee2342c869a988ff0fc21637b82
andres@digitalhouse.com <andres@digitalhouse.com>
2018-05-31 21:02:01 -0400

    Mumuki Export on 2018-05-31 21:02:01 -0400

--- 00021_¿Cuál es mayor?/test..js
+++ 00021_¿Cuál es mayor?/test..js
@@ -1,5 +1,14 @@
 describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+  it("elMayor(10, 9)", function() {
+    assert.equal(elMayor(10, 9), 10)
+  });
+  it("elMayor(5, 11)", function() {
+    assert.equal(elMayor(5, 11), 11)
+  });
+  it("elMayor(4, 4)", function() {
+    assert.equal(elMayor(4, 4), 4)
+  });
+  it("elMayor(2, 120)", function() {
+    assert.equal(elMayor(2, 120), 120)
+  });
 })
