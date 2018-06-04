59b39f736801513747903a8027040e9a9a47e9e3
andres@digitalhouse.com <andres@digitalhouse.com>
2018-06-04 11:34:26 -0400

    Mumuki Export on 2018-06-04 11:34:26 -0400

--- 00025_El filósofo hipster/test..js
+++ 00025_El filósofo hipster/test..js
@@ -1,5 +1,11 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+describe("Filósofo Hipster", function() {
+  it("filosofoHipster('Músico', 'Argentina', 5)", function() {
+    assert.equal(filosofoHipster('Músico', 'Argentina', 5), true)
+  });
+   it("filosofoHipster('Futbolista', 'Brasil', 12)", function() {
+    assert.equal(filosofoHipster('Futbolista', 'Brasil', 12), false)
+  });
+  it("filosofoHipster('Músico', 'Argentina', 1)", function() {
+    assert.equal(filosofoHipster('Músico', 'Argentina', 1), false)
+  });
 })
