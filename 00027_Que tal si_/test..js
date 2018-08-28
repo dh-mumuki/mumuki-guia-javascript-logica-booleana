bc6827fad28f27dd8e42ed963e18cf33ef8f0a71
andres@digitalhouse.com <andres@digitalhouse.com>
2018-08-28 14:39:24 -0400

    Mumuki Export on 2018-08-28 14:39:24 -0400

--- 00027_Que tal si_/test..js
+++ 00027_Que tal si_/test..js
@@ -1,6 +1,8 @@
 describe("", function() {
   it("Declarada y asignada la variable diaDeSemana", function() {
-    assert.Equal(diaDeSemana, "domingo");
-    
+    assert.equal(diaDeSemana, "domingo");
+  });
+    it("deberia imprimir ''Hoy se juega al futbol!!!''", function() {
+    assert.equal(console.toString(), "Hoy se juega al futbol");
   });
 })
