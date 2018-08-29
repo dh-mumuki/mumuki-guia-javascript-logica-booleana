9f1c687008d16193037b7c1624cc10ac0a0c7d1e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-08-29 10:19:55 -0400

    Mumuki Export on 2018-08-29 10:19:55 -0400

--- extra.js
+++ extra.js
@@ -1,7 +1,7 @@
 var fakeConsole = {
   lines: [],
   log: function(line) {
-    this.lines.push(line + "\n");
+    this.lines.push(line);
   },
   toString: function() {
     return String.prototype.concat.apply([], this.lines);
