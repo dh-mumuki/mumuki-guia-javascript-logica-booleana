8db1c168b8489c576c98bc4a0aafa8a4daed6f40
andres@digitalhouse.com <andres@digitalhouse.com>
2018-08-29 00:34:02 -0400

    Mumuki Export on 2018-08-29 00:34:02 -0400

--- 00028_Y ¿que tal si no____/test..js
+++ 00028_Y ¿que tal si no____/test..js
@@ -1,9 +1,9 @@
 describe("", function() {
     it("hoySeJuega(\"domingo\") debería decir que se juega ", function() {
-    assert.equal(hoySeJuega("domingo"), "Hoy se juega al futbol!!!\n");
+    assert.equal(hoySeJuega("domingo"), "Hoy se juega al futbol!!!");
   });
   it("hoySeJuega(\"lunes\") debería decir que no se juega ", function() {
     hoySeJuega("lunes")
-    assert.equal(hoySeJuega("lunes"), "Hoy no se juega al futbol :(\n");
+    assert.equal(hoySeJuega("lunes"), "Hoy no se juega al futbol :(");
   });
 })
