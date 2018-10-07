38698a834565e0b85cd33e69532ffde5f36e561a
jfermoso@digitalhouse.com <jfermoso@digitalhouse.com>
2018-10-07 17:06:13 -0400

    Mumuki Export on 2018-10-07 17:06:13 -0400

--- 00007_La familia Buen Día/test..js
+++ 00007_La familia Buen Día/test..js
@@ -1,15 +1,15 @@
 it("arcadio y aurelianoJose son medios hermanos", function() {
-  assert.equal(sonMediosHermanos(arcadio, aurelianoJose), true);
+  assert.equal(sonMedioHermanos(arcadio, aurelianoJose), true);
 })
 
 it("aurelianoSegundo y remedios no son medios hermanos", function() {
-  assert.equal(sonMediosHermanos(aurelianoSegundo, remedios), false);
+  assert.equal(sonMedioHermanos(aurelianoSegundo, remedios), false);
 })
 
 it("aurelianoSegundo y aurelianoJose no son medios hermanos", function() {
-  assert.equal(sonMediosHermanos(aurelianoSegundo, aurelianoJose), false);
+  assert.equal(sonMedioHermanos(aurelianoSegundo, aurelianoJose), false);
 })
 
 it("remedios y arcadio no son medios hermanos", function() {
-  assert.equal(sonMediosHermanos(remedios, arcadio), false);
+  assert.equal(sonMedioHermanos(remedios, arcadio), false);
 })
